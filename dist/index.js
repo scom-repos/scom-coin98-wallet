3228cebc496e2eaf349633b507ecfd1291578d0d
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2023-04-03 18:45:05 +0800

    first commit

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+define("@scom/scom-coin98-wallet", ["require", "exports", "@ijstech/eth-wallet", "@ijstech/components"], function (require, exports, eth_wallet_1, components_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.Coin98Provider = void 0;
+    let moduleDir = components_1.application.currentModuleDir;
+    function fullPath(path) {
+        if (path.indexOf('://') > 0)
+            return path;
+        return `${moduleDir}/${path}`;
+    }
+    class Coin98Provider extends eth_wallet_1.EthereumProvider {
+        get displayName() {
+            return 'Coin98 Wallet';
+        }
+        get image() {
+            return fullPath('img/Coin98.svg');
+        }
+        get provider() {
+            return window['ethereum'];
+        }
+        get homepage() {
+            return 'https://docs.coin98.com/products/coin98-wallet';
+        }
+        installed() {
+            let ethereum = window['ethereum'];
+            return !!ethereum && (!!ethereum.isCoin98 || !!window['isCoin98']);
+        }
+    }
+    exports.Coin98Provider = Coin98Provider;
+});
---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import { EthereumProvider } from '@ijstech/eth-wallet';
+export declare class Coin98Provider extends EthereumProvider {
+    get provider(): any;
+    get homepage(): string;
+    installed(): boolean;
+}
