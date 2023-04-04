00c8b39d353232115f17726df9c939e14be2b037
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2023-04-04 11:42:25 +0800

    Remove types, update dependencies

--- types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { EthereumProvider } from '@ijstech/eth-wallet';
-export declare class Coin98Provider extends EthereumProvider {
-    get provider(): any;
-    get homepage(): string;
-    installed(): boolean;
-}
